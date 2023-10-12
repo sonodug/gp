3b4a5b7278e2654dcc5d4ef323a5a3aa82b6b594
sonodug <danilsonodug@gmail.com>
2023-10-12 21:20:19 +0300

    Update.........

--- DAG/main_dag.py
+++ DAG/main_dag.py
@@ -7,21 +7,28 @@ from airflow.models import Variable
 
 DB_CONN = "gp_std3_47"
 DB_SCHEMA = 'std3_47'
+
 DB_PROC_FULL_LOAD = 'f_load_full'
 DB_PROC_PART_LOAD = 'f_load_simple_partition'
+DB_UPSERT_LOAD = 'f_load_simple_upsert'
 DB_PROC_LOAD_MART = 'f_calculate_plan_mart'
+
 FULL_LOAD_TABLES = ['region', 'chanel', 'price', 'product']
 PART_LOAD_TABLES = ['sales']
+UPS_LOAD_TABLES = ['plan']
+
 FULL_LOAD_FILES = {
     'region': 'region',
     'chanel': 'chanel',
     'price': 'price',
     'product': 'product',
 }
+
 MD_TABLE_FULL_LOAD_QUERY = (
     f"select {DB_SCHEMA}.{DB_PROC_FULL_LOAD}(%(tab_name)s, %(file_name)s);"
 )
 MD_TABLE_PART_LOAD_QUERY = f"select {DB_SCHEMA}.{DB_PROC_PART_LOAD}(%(tab_name)s, %(date_attr)s, %(start_date)s, %(end_date)s, %(source_tab)s, %(user)s, %(pass)s);"
+MD_TABLE_UPSERT_LOAD_QUERY = f"select {DB_SCHEMA}.{DB_UPSERT_LOAD}(%(source_tab_name)s, %(tab_name)s, %(date_attr)s, %(user)s, %(pass)s);"
 MD_LOAD_MART_JAN = f"select {DB_SCHEMA}.{DB_PROC_LOAD_MART}('202101');"
 
 default_args = {
@@ -70,9 +77,24 @@ with DAG(
                 },
             )
 
+    with TaskGroup("delta_ups_load") as task_ups_load:
+        for table in UPS_LOAD_TABLES:
+            task = PostgresOperator(
+                task_id=f"load_table_fact_{table}",
+                postgres_conn_id=DB_CONN,
+                sql=MD_TABLE_UPSERT_LOAD_QUERY,
+                parameters={
+                    'source_tab_name': 'gp.plan',
+                    'tab_name': f'{DB_SCHEMA}.{table}',
+                    'date_attr': 'date',
+                    'user': 'intern',
+                    'pass': 'intern',
+                },
+            )
+
     task_load_mart = PostgresOperator(
         task_id='start_calculate_mart', postgres_conn_id=DB_CONN, sql=MD_LOAD_MART_JAN
     )
     task_end = DummyOperator(task_id="end")
 
-    task_start >> task_full_load >> task_part_load >> task_load_mart >> task_end
+    task_start >> task_full_load >> task_part_load >> task_ups_load >> task_load_mart >> task_end
--- Script.sql
+++ Script.sql
@@ -9,5 +9,15 @@ TRUNCATE TABLE std3_47.sales;
 TRUNCATE TABLE std3_47.price;
 TRUNCATE TABLE std3_47.product;
 
+
+SELECT std3_47.f_load_simple_partition('std3_47.sales', '"date"', '2021-01-12', '2021-02-01', 'gp.sales', 'intern', 'intern');
+
+
+SELECT std3_47.f_load_simple_upsert('gp.plan', 'std3_47.plan', '"date"', 'intern', 'intern');
+
+SELECT s."date" FROM std3_47.sales s
+JOIN std3_47.plan p ON p.date = s."date"; 
+
+
 SELECT std3_47.f_load_full('std3_47.price', 'price');
 SELECT std3_47.f_load_full('std3_47.region', 'region');
